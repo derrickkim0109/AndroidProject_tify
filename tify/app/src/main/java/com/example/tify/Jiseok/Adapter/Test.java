3b0463f54c3bb53c0cfb163f8d2e002b9a9438d8
derrickkim <olaaivl@naver.com>
2021-01-06 16:33:03 +0900

    test

---
+++ tify/app/src/main/java/com/example/tify/Jiseok/Adapter/Test.java
@@ -0,0 +1,13 @@
+package com.example.tify;
+
+
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      
+    }
+}
