c6579348dd590648c8391773d5441e99c6c03c48
DESKTOP-O18AQJ8\choi_hyeona <hyeona_c@naver.com>
2021-01-07 14:56:25 +0900

    현아 - 앱바

--- tify/app/src/main/java/com/example/tify/MainActivity.java
+++ tify/app/src/main/java/com/example/tify/MainActivity.java
@@ -1,20 +1,103 @@
 package com.example.tify;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.tify.Taehyun.Activity.Mypage;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView; // 바텀네비게이션 뷰
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      ImageView backgroundImageView = findViewById(R.id.gifTest);
+
+
+        ImageView backgroundImageView = findViewById(R.id.gifTest);
         Glide.with(this).load(R.raw.gps_setting).into(backgroundImageView);
+        //gif를 사용하기위함
+
+        //////////////////////////////////////////////////////////////////////////////////
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        //하단 네비게이션 선언
+        menu = bottomNavigationView.getMenu();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) { //menu_bottom.xml에서 지정해줬던 아이디 값을 받아와서 각 아이디값마다 다른 이벤트를 발생시킵니다.//
+                    case R.id.action1: {
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        //홈버튼
+                        return true;
+                    }
+                    case R.id.action2: {
+
+                        return true;
+                    }
+                    case R.id.action3: {
+
+                        return true;
+                    }
+                    case R.id.action4: {
+
+                        return true;
+                    }
+                    case R.id.action5: {
+
+                        return true;
+                    }
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.show();
+        // Custom Actionbar를 사용하기 위해 CustomEnabled을 true 시키고 필요 없는 것은 false 시킨다
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+
+        //layout을 가지고 와서 actionbar에 포팅을 시킵니다.
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.custom_actionbar, null);
+
+        actionBar.setCustomView(actionbar);
+        TextView title = findViewById(R.id.title);
+        title.setText("여기에 각기의 제목을 입력해야함");
+//        ImageButton cart = findViewById(R.id.cart);
+//        cart.setVisibility(View.GONE);0-ㅔ9
+//         장바구니 없애려면 위에거 살리면 됨
+        //액션바 양쪽 공백 없애기
+        Toolbar parent = (Toolbar) actionbar.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+        return true;
     }
 }
+//////////////////////////////////////////////////////////////////////////////////
