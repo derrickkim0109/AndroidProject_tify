dc6a3f690144bdeee9fa8a52f2130a15a2de4f4f
CHOIJISEOK318 <wltjr318@naver.com>
2021-01-11 02:34:49 +0900

    회원가입 완성

---
+++ tify/app/src/main/java/com/example/tify/Jiseok/NetworkTask/CJS_NetworkTask.java
@@ -0,0 +1,176 @@
+package com.example.tify.Jiseok.NetworkTask;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.tify.Hyeona.Bean.Bean_review_review;
+import com.example.tify.Hyeona.Bean.Bean_review_store;
+import com.example.tify.Minwoo.Bean.Menu;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class CJS_NetworkTask extends AsyncTask<Integer, String, Object> {
+
+    final static String TAG = "CUDNetworkTask";
+    Context context = null;
+    String mAddr = null;
+    ProgressDialog progressDialog = null;
+    String where = null;
+
+    int userTelCount = 0;
+    int userEmailCount=0;
+
+
+    public CJS_NetworkTask(Context context, String mAddr, String where) {
+        this.context = context;
+        this.mAddr = mAddr;
+        this.where = where;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Log.v(TAG, "onPreExecute()");
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(progressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("Create/Update/Delete");
+        progressDialog.setMessage("Working...");
+        progressDialog.show();
+    }
+
+    @Override
+    protected Object doInBackground(Integer... integers) {
+        Log.v(TAG, "doInBackground()");
+
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+
+        String result = null;
+
+        try {
+            URL url = new URL(mAddr);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(10000);
+
+            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                inputStream = httpURLConnection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                bufferedReader = new BufferedReader(inputStreamReader);
+
+                while (true){
+                    String strline = bufferedReader.readLine();
+                    if(strline == null) break;
+                    stringBuffer.append(strline + "\n");
+                }
+
+                switch (where){
+                    case "userTelCount":
+                        userTelCount(stringBuffer.toString());
+                        break;
+                    case "userEmailCount":
+                        userEmailCount(stringBuffer.toString());
+                        break;
+                }
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if(bufferedReader != null) bufferedReader.close();
+                if(inputStreamReader != null) inputStreamReader.close();
+                if(inputStream != null) inputStream.close();
+
+            }catch (Exception e2){
+                e2.printStackTrace();
+            }
+        }
+
+        switch (where){
+            case "userTelCount":
+                return userTelCount;
+            case "userEmailCount":
+                return userEmailCount;
+        }
+
+
+
+       return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        Log.v(TAG, "onProgressUpdate()");
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        Log.v(TAG, "onPostExecute()");
+        super.onPostExecute(o);
+        progressDialog.dismiss();
+
+    }
+
+    @Override
+    protected void onCancelled() {
+        Log.v(TAG, "onCancelled");
+        super.onCancelled();
+    }
+
+
+
+
+    private void userTelCount(String s){
+        //Log.v("aaaaaa","parser()");
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("user_info"));
+
+            for (int i=0; i<jsonArray.length(); i++){
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+                int storekeeper_skSeqNo = jsonObject1.getInt("userTelCount");
+
+                userTelCount = storekeeper_skSeqNo;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void userEmailCount(String s){
+        //Log.v("aaaaaa","parser()");
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("user_info"));
+
+            for (int i=0; i<jsonArray.length(); i++){
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+                int storekeeper_skSeqNo = jsonObject1.getInt("EmailCount");
+
+                userEmailCount = storekeeper_skSeqNo;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    } // ------
+
+
