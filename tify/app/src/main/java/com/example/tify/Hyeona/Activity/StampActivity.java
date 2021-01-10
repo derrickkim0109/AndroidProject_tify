97e0e97598980d488b5f7de30dea17c442944813
DESKTOP-O18AQJ8\choi_hyeona <hyeona_c@naver.com>
2021-01-10 21:32:24 +0900

    11

--- tify/app/src/main/java/com/example/tify/Hyeona/Activity/StampActivity.java
+++ tify/app/src/main/java/com/example/tify/Hyeona/Activity/StampActivity.java
@@ -18,8 +18,7 @@ public class StampActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stamp);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
