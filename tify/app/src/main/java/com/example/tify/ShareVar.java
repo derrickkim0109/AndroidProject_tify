90099f4abc146b46c1f04a0f5b3c4142f2609ab2
CHOIJISEOK318 <wltjr318@naver.com>
2021-01-13 19:15:00 +0900

    충돌수정/최지석

--- tify/app/src/main/java/com/example/tify/ShareVar.java
+++ tify/app/src/main/java/com/example/tify/ShareVar.java
@@ -2,11 +2,9 @@ package com.example.tify;
 
 public class ShareVar {
 
-<<<<<<< HEAD
+
     final static String macIP = "192.168.219.100";
-=======
-    final static String macIP = "192.168.0.55";
->>>>>>> 7d548c0ee00feb1be8eb3139488640bb1c6bd655
+
 
     public ShareVar() {
 
