8cbc2d489ee52d7e170796e3ad2d39e2a4b34a47
CHOIJISEOK318 <wltjr318@naver.com>
2021-01-14 14:23:07 +0900

    01-14 / 최지석 / 결제비밀번호입력액티비티 생성

---
+++ tify/app/src/main/java/com/example/tify/Jiseok/Activity/PaymentPayPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.tify.Jiseok.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tify.R;
+
+public class PaymentPayPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cjs_activity_payment_pay_password);
+    }
+}
